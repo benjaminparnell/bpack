c189b71ab3f121e8d8eb90a6ff70bbde14072d14
Ben Parnell <benjaminparnell.94@gmail.com>
2014-04-29 19:51:21 +0100

    Cleaned up code, removed underscore dep

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
 #!/usr/bin/env node
-
 var fs = require('fs')
-  , _ = require('underscore')
   , async = require('async')
   , scripts = null
   , pkg = null
 
 async.waterfall([
-  function pkgExists(callback) {
+  function pkgExists(cb) {
     fs.exists(process.cwd () + '/package.json', function (exists) {
       if (exists) {
         pkg = require(process.cwd() + '/package.json')
         scripts =  pkg.scripts
-        callback(null)
+        cb(null)
       } else {
-        console.log('No package.json found in your project')
-        process.exit(1)
+        cb('No package.json found in your project')
       }
     })
   },
-  function getBinaries(callback) {
+  function getBinaries(cb) {
     fs.readdir(process.cwd() + '/node_modules/.bin', function (err, files) {
       if (err) {
-        console.log(err.toString())
-        process.exit(1)
+        cb(err.toString())
+      } else if (files.length === 0) {
+        cb('Nothing in ./node_modules/.bin')
       } else {
-        callback(null, files)
+        cb(null, files)
       }
     })
   },
   function useBinaries(bin, cb) {
-    _(Object.keys(scripts)).each(function (key) {
-      var value = _.map(scripts[key].split(' '), function (item) {
-        if (_.contains(bin, item)) {
+    Object.keys(scripts).forEach(function (key) {
+      scripts[key] = scripts[key].split(' ').map(function (item) {
+        if (bin.indexOf(item) > -1) {
           return './node_modules/.bin/' + item
         }
         return item
-      })
-      scripts[key] = value.join(' ')
+      }).join(' ')
     })
     cb(null, pkg)
-  }
-  ], function (err, res) {
+  }], function (err, res) {
+    if (err) {
+      console.log(err)
+      process.exit(1)
+    }
     fs.writeFile(process.cwd() + '/package.json', JSON.stringify(res, null, 2), {encoding: 'utf8'}, function (err) {
       if (err) throw err
       console.log('./node_modules/.bin paths written to package.json scripts.')
